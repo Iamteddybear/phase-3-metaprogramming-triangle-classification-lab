298d6605ca8fd3759938ddf04c5172b9cec2d04c
ned <nedschultz@outlook.com>
2023-04-10 12:32:06 +1000

    commit tree de308ce9805e95da3a0e33840d5b1a9c44b1a9e8

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+   attr_reader :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+    raise TriangleError if !valid_triangle?
+  end
+
+  def kind
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  private
+
+  def valid_triangle?
+    a > 0 && b > 0 && c > 0 && a + b > c && b + c > a && a + c > b
+  end
+
+  class TriangleError < StandardError
+    def message
+      "This is not a valid triangle."
+    end
+  end
 end
